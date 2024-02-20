c055582b2483922e6147eb1faa5eac7fbf0897b9
Crowdbotics <team@crowdbotics.com>
2024-02-20 23:10:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "night-life-2-46515",
+  url: "https://night-life-2-46515.botics.co",
+  api: "https://night-life-2-46515.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
